1646600e47bca9909805df6c4965c843184aad56
Ian Kinner & Menuka Samaranayake & Prince Sadie <pair+ian-kinner.menuka-s.princesadie@devbootcamp.com>
2016-03-17 20:52:31 -0500

    so close.. we can taste it.. and it tastes like.. a monitor

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -41,3 +41,25 @@ get '/places' do
 
   data.to_json
 end
+
+get '/places2' do
+  data = []
+  bars_with_specials_today = []
+  day_num = Time.new.wday + 1
+
+  # use array to drop duplicates
+  Day.find(day_num).specials.each do |special|
+    bars_with_specials_today[special.bar_id] = 1
+  end
+
+  max_distance = 1000 # meters
+  nearby_bars_with_dist = find_bars_by_dist(bars_with_specials_today, max_distance)
+
+  nearby_bars_with_dist.each do |bar_dist|
+    bar = bar_dist[0]
+    # data << [bar.name, "Chicago, IL", ""]
+    data << [bar.name, (bar.address + "Chicago, IL"), ""]
+  end
+
+  data.to_json
+end
